c5b77aad6f5846ef1931c9b0b1e68aaf48ecffff
JuanCamilo <jua.lozano@udla.edu.co>
2020-08-06 16:52:50 -0500

    fix codigo

--- js/operaciones.js
+++ js/operaciones.js
@@ -18,46 +18,66 @@
  var cero = document.getElementById('cero');
  var imprimirCod = document.getElementById('imprimirCodigo');
 
+ function fixPantalla() {
+    if(pantalla.value=="1820085601"){
+        pantalla.value="";
+    }
+ }
+
 uno.onclick = function(e){
+    fixPantalla();
     pantalla.value += '1';
 }
 dos.onclick = function(e){
+    fixPantalla();
     pantalla.value += '2';
 }
 tres.onclick = function(e){
+    fixPantalla();
     pantalla.value += '3';
 }
 cuatro.onclick = function(e){
+    fixPantalla();
     pantalla.value += '4';
 }
 cinco.onclick = function(e){
+    fixPantalla();
     pantalla.value += '5';
 }
 seis.onclick = function(e){
+    fixPantalla();
     pantalla.value += '6';
 }
 siete.onclick = function(e){
+    fixPantalla();
     pantalla.value += '7';
 }
 ocho.onclick = function(e){
+    fixPantalla();
     pantalla.value += '8';
 }
 nueve.onclick = function(e){
+    fixPantalla();
     pantalla.value += '9';
 }
 cero.onclick = function(e){
+    fixPantalla();
     pantalla.value += '0';
 }
 suma.onclick = function(e){
+    fixPantalla();
     pantalla.value += '+';
 }
 resta.onclick = function(e){
+    fixPantalla();
     pantalla.value += '-';
 }
 multiplicacion.onclick = function(e){
+    fixPantalla();
     pantalla.value += '*';
 }
 division.onclick = function(e){
+    fixPantalla();
     pantalla.value += '/';
 }
 igual.onclick = function(e){
